945438cc15058ce6b1ab8b856a574ab9a98f5f58
Peter Mackay <git@petmac.dev>
2024-06-12 23:15:37 +0100

    Parse command line

--- tools/tmx2map/src/main.rs
+++ tools/tmx2map/src/main.rs
@@ -1,3 +1,16 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    input_tmx_path: PathBuf,
+    output_map_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("Input:  {}", cli.input_tmx_path.display());
+    println!("Output: {}", cli.output_map_path.display());
 }
