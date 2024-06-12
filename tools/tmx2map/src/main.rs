451171428dc94f3c32e8cbf1e42795820a619f45
Peter Mackay <git@petmac.dev>
2024-06-12 23:32:38 +0100

    Load Tiled map

--- tools/tmx2map/src/main.rs
+++ tools/tmx2map/src/main.rs
@@ -1,6 +1,7 @@
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf};
 
 use clap::Parser;
+use tiled::Loader;
 
 #[derive(Parser)]
 struct Cli {
@@ -8,9 +9,14 @@ struct Cli {
     output_map_path: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     println!("Input:  {}", cli.input_tmx_path.display());
     println!("Output: {}", cli.output_map_path.display());
+
+    let mut loader = Loader::new();
+    let _tmx = loader.load_tmx_map(cli.input_tmx_path)?;
+
+    Ok(())
 }
