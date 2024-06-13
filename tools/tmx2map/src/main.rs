f400b76f5759e9e3c8a44229356113b4570bbf5f
Peter Mackay <git@petmac.dev>
2024-06-14 00:24:18 +0100

    Start loading tileset and iterating through tiles

--- tools/tmx2map/src/main.rs
+++ tools/tmx2map/src/main.rs
@@ -1,13 +1,19 @@
-use std::{error::Error, path::PathBuf};
+use std::{error::Error, fs::File, path::PathBuf};
 
 use clap::Parser;
-use tiled::Loader;
+use tiled::{Loader, Tileset};
 
 #[derive(Parser)]
 struct Cli {
     input_tmx_path: PathBuf,
     output_map_path: PathBuf,
 }
+
+struct TilesetImages {
+    tiles: Vec<TileImage>,
+}
+
+struct TileImage {}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
@@ -16,7 +22,42 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Output: {}", cli.output_map_path.display());
 
     let mut loader = Loader::new();
-    let _tmx = loader.load_tmx_map(cli.input_tmx_path)?;
+    let tmx = loader.load_tmx_map(cli.input_tmx_path)?;
+
+    let tileset_images: Vec<TilesetImages> = tmx
+        .tilesets()
+        .iter()
+        .map(|tileset| load_tileset(tileset.as_ref()).unwrap())
+        .collect();
+
+    for layer in tmx.layers().filter_map(|layer| layer.as_tile_layer()) {
+        let layer_w = layer.width().ok_or("Layer has no width")?;
+        let layer_h = layer.height().ok_or("Layer has no height")?;
+
+        for tile_row in 0..layer_h {
+            for tile_col in 0..layer_w {
+                let layer_tile = match layer.get_tile(tile_col as i32, tile_row as i32) {
+                    Some(layer_tile) => layer_tile,
+                    None => continue,
+                };
+                let tileset_images = &tileset_images[layer_tile.tileset_index()];
+                let tile_image = &tileset_images.tiles[layer_tile.id() as usize];
+                todo!()
+            }
+        }
+    }
 
     Ok(())
+}
+
+fn load_tileset(tileset: &Tileset) -> Result<TilesetImages, Box<dyn Error>> {
+    let image = tileset.image.as_ref().ok_or("Tileset has no image")?;
+    let path = image.source.canonicalize()?;
+    let decoder = png::Decoder::new(File::open(path)?);
+    let mut reader = decoder.read_info()?;
+    let mut buf = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut buf)?;
+    let bytes = &buf[..info.buffer_size()];
+
+    Ok(TilesetImages { tiles: Vec::new() })
 }
