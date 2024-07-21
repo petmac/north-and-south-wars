75381746c9072ffb3095dc8dccac383f476e0cad
Peter Mackay <git@petmac.dev>
2024-07-21 13:12:25 +0100

    Move tile size to map

--- game/game/include/game/map.h
+++ game/game/include/game/map.h
@@ -4,6 +4,8 @@
 
 constexpr u16 maxMapWidth = 30;
 constexpr u16 maxMapHeight = 20;
+constexpr u16 tileWidth = 16;
+constexpr u16 tileHeight = 16;
 
 struct Map {
   u8 width;
--- game/main/tileset.h
+++ game/main/tileset.h
@@ -3,9 +3,8 @@
 #include "playfield.h"
 
 #include "amiga/bitmap.h"
+#include "game/map.h"
 
-constexpr u16 tileWidth = 16;
-constexpr u16 tileHeight = 16;
 constexpr u16 tileCount = 256;
 
 using TileBitmap = InterleavedBitmap<tileWidth, tileHeight, playfieldDepth>;
