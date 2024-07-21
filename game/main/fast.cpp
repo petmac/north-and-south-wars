aace99a1c2700fbde98e9563c2220efaa2825e86
Peter Mackay <git@petmac.dev>
2024-07-21 13:25:03 +0100

    Centre mouse on init

--- game/main/fast.cpp
+++ game/main/fast.cpp
@@ -1,5 +1,8 @@
 #include "fast.h"
 
+#include "amiga/custom.h"
+#include "game/screen.h"
+
 static void initFrameFast(FrameFast &frameFast) {
   frameFast.state = FrameState::cleared;
   frameFast.intro.linesDrawn = 0;
@@ -7,8 +10,14 @@ static void initFrameFast(FrameFast &frameFast) {
 }
 
 void initFast(Fast &fast) {
+  const u16 mouseData = custom.joy0dat;
+
   fast.game = initGame();
   fast.backBufferIndex = 0;
+  fast.lastMouseDataX = mouseData & 0xff;
+  fast.lastMouseDataY = (mouseData >> 8) & 0xff;
+  fast.mouseX = screenWidth / 2;
+  fast.mouseY = screenHeight / 2;
 
   for (u16 frameIndex = 0; frameIndex < 2; ++frameIndex) {
     initFrameFast(fast.frames[frameIndex]);
