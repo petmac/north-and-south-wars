ff7a7565b46a78c465668f1501f73f9566cb5295
Peter Mackay <git@petmac.dev>
2024-08-02 23:53:55 +0100

    Build unit positions from map

--- tools/tmx2map/src/main.rs
+++ tools/tmx2map/src/main.rs
@@ -35,25 +35,38 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut loader = Loader::new();
     let tmx = loader.load_tmx_map(cli.input_tmx_path)?;
-    let layer = tmx
-        .layers()
-        .filter_map(|layer| layer.as_tile_layer())
-        .next()
-        .ok_or("No tile layers in map")?;
+    let mut tile_layers = tmx.layers().filter_map(|layer| layer.as_tile_layer());
+    let tiles_layer = tile_layers.next().ok_or("No tiles layer in map")?;
+    let units_layer = tile_layers.next().ok_or("No units layer in map")?;
 
-    let width = layer.width().ok_or("Layer has no width")? as usize;
-    let height = layer.height().ok_or("Layer has no height")? as usize;
+    let width = tiles_layer.width().ok_or("Layer has no width")? as usize;
+    let height = tiles_layer.height().ok_or("Layer has no height")? as usize;
     let mut tile_indices: [[u8; MAX_MAP_WIDTH]; MAX_MAP_HEIGHT] =
         [[0; MAX_MAP_WIDTH]; MAX_MAP_HEIGHT];
+    let mut units = [TileCoords { column: 0, row: 0 }; MAX_UNITS];
+    let mut unit_count = 0;
 
     for tile_row in 0..height {
         for tile_col in 0..width {
-            let layer_tile = match layer.get_tile(tile_col as i32, tile_row as i32) {
+            let tiles_layer_tile = match tiles_layer.get_tile(tile_col as i32, tile_row as i32) {
                 Some(layer_tile) => layer_tile,
                 None => continue,
             };
-            let tile_index = layer_tile.id() as u8;
+            let tile_index = tiles_layer_tile.id() as u8;
             tile_indices[tile_row][tile_col] = tile_index;
+
+            match units_layer.get_tile(tile_col as i32, tile_row as i32) {
+                Some(layer_tile) => {
+                    let unit_index = layer_tile.id() as u8;
+                    let coords = TileCoords {
+                        column: tile_col as u8,
+                        row: tile_row as u8,
+                    };
+                    units[unit_count] = coords;
+                    unit_count += 1;
+                }
+                None => {}
+            };
         }
     }
 
@@ -61,8 +74,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         width: width as u8,
         height: height as u8,
         tile_indices,
-        unit_count: 0,
-        units: [TileCoords { column: 0, row: 0 }; MAX_UNITS],
+        unit_count: unit_count as u8,
+        units,
     };
 
     let mut output_file = File::create(cli.output_map_path)?;
