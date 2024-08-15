473f73e9db31e07378e5601a81c1bd4c098082ac
Peter Mackay <git@petmac.dev>
2024-08-15 22:34:32 +0100

    Add link to A* guide

--- game/game/src/pathfinding.cpp
+++ game/game/src/pathfinding.cpp
@@ -4,6 +4,9 @@
 
 void findPath(Pathfinding &pathfinding, const TileCoords &from,
               const TileCoords &to) {
+  // TODO This is for first pass display
   pathfinding.destination = to;
+
   // https://www.redblobgames.com/pathfinding/a-star/introduction.html
+  // https://www.redblobgames.com/pathfinding/a-star/implementation.html
 }
