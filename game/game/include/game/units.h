f1b8ce4cb24d7ed7e0689da9918eec9eccd91cff
Peter Mackay <git@petmac.dev>
2024-08-17 22:42:05 +0100

    Add table of unit definitions

--- game/game/include/game/units.h
+++ game/game/include/game/units.h
@@ -5,10 +5,39 @@
 constexpr u16 unitWidth = 16;
 constexpr u16 unitHeight = 16;
 
+// https://warswiki.org/wiki/Advance_Wars#Units
 enum class UnitType : u8 {
   infantry,
   mech,
   lightTank,
   mediumTank,
   count,
+};
+
+struct UnitDef {
+  u16 movement;
+};
+
+// https://warswiki.org/wiki/Advance_Wars#Units
+constexpr UnitDef unitDefs[static_cast<u16>(UnitType::count)] = {
+    // Infantry
+    // https://warswiki.org/wiki/Infantry_(unit)
+    {
+        .movement = 3,
+    },
+    // Mech
+    // https://warswiki.org/wiki/Mechanised_Infantry
+    {
+        .movement = 2,
+    },
+    // Light tank
+    // https://warswiki.org/wiki/Tank
+    {
+        .movement = 6,
+    },
+    // Medium tank
+    // https://warswiki.org/wiki/Medium_Tank
+    {
+        .movement = 5,
+    },
 };
