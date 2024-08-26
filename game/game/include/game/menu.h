059ce9a4b306c57bc8a5ef854964a61d39b4b7f2
Peter Mackay <git@petmac.dev>
2024-08-26 22:41:09 +0100

    Move menu

--- game/game/include/game/menu.h
+++ game/game/include/game/menu.h
@@ -5,5 +5,5 @@
 // Conveniently a multiple of tile size
 constexpr u16 menuButtonWidth = 64;
 constexpr u16 menuButtonHeight = 16;
-constexpr u16 menuX = 64;
-constexpr u16 menuY = 64;
+constexpr u16 menuX = 128;
+constexpr u16 menuY = 80;
