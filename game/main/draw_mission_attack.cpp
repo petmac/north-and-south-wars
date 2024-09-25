97a17ef61423f58084aea20f9b5defd0a8612e96
Peter Mackay <git@petmac.dev>
2024-09-25 23:42:56 +0100

    Restore background slices before drawing units

--- game/main/draw_mission_attack.cpp
+++ game/main/draw_mission_attack.cpp
@@ -6,29 +6,52 @@
 
 #include "game/screen.h" // screenWidth
 
-static void drawBackground(Background &dst, const AttackBackground &src,
-                           u16 xWords) {
-  for (u16 sliceIndex = 0, y = 0; sliceIndex < attackBackgroundSliceCount;
+static void drawBackgroundSlices(Background &dst, const AttackBackground &src,
+                                 u16 xWords, u16 firstSlice,
+                                 u16 sliceAfterLast) {
+  for (u16 sliceIndex = firstSlice,
+           y = firstSlice * attackBackgroundSliceHeight;
+       sliceIndex < sliceAfterLast;
        ++sliceIndex, y += attackBackgroundSliceHeight) {
     blitFast(dst, src[sliceIndex], xWords, y);
   }
 }
 
+static void drawBackground(Background &dst, const AttackBackground &src,
+                           u16 xWords) {
+  drawBackgroundSlices(dst, src, xWords, 0, attackBackgroundSliceCount);
+}
+
 void drawMissionAttack(Background &background, FrameFast &frameFast,
                        const Mission &mission) {
+  // TODO Get these from the terrain types.
+  const AttackBackground &leftBackground =
+      chip.mission.attack.backgrounds.plain;
+  const AttackBackground &rightBackground =
+      chip.mission.attack.backgrounds.mountain;
+  constexpr u16 leftXWords = 0;
+  constexpr u16 rightXWords = screenWidthInWords / 2;
+
   // Draw the background if needed.
   switch (frameFast.state) {
-  case FrameState::drawnMissionAttack:
-    // Already drawn
-    break;
+  case FrameState::drawnMissionAttack: {
+    // Already drawn similar, so just overwrite the animating parts
+    constexpr u16 firstSlice = (attackBackgroundSliceCount / 2) - 1;
+    constexpr u16 sliceAfterLast = firstSlice + 2;
+    drawBackgroundSlices(background, leftBackground, leftXWords, firstSlice,
+                         sliceAfterLast);
+    drawBackgroundSlices(background, rightBackground, rightXWords, firstSlice,
+                         sliceAfterLast);
+  } break;
   default:
-    // Need to draw the background
-    drawBackground(background, chip.mission.attack.backgrounds.plain, 0);
-    drawBackground(background, chip.mission.attack.backgrounds.mountain,
-                   screenWidthInWords >> 1);
+    // Draw the whole background
+    drawBackground(background, leftBackground, leftXWords);
+    drawBackground(background, rightBackground, rightXWords);
     frameFast.state = FrameState::drawnMissionAttack;
-    blitFast(background, chip.mission.attack.units.forces[0].units[0], 5, 128);
-    blitFast(background, chip.mission.attack.units.forces[1].units[0], 15, 128);
     break;
   }
+
+  // Draw the units
+  blitFast(background, chip.mission.attack.units.forces[0].units[0], 5, 128);
+  blitFast(background, chip.mission.attack.units.forces[1].units[0], 15, 128);
 }
