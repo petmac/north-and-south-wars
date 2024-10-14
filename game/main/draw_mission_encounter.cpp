1caa96d6c5004100ac194889cae2066456b3be0b
Peter Mackay <git@petmac.dev>
2024-10-15 00:03:04 +0100

    Compute background from unit terrain

--- game/main/draw_mission_encounter.cpp
+++ game/main/draw_mission_encounter.cpp
@@ -4,7 +4,34 @@
 #include "chip.h"
 #include "frame_fast.h"
 
-#include "game/screen.h" // screenWidth
+#include "game/mission/mission.h" // Mission
+#include "game/mission/terrain.h" // Terrain
+#include "game/screen.h"          // screenWidth
+
+static const EncounterBackground &terrainBackground(Terrain terrain) {
+  const EncounterBackgrounds &backgrounds = chip.mission.encounter.backgrounds;
+  const EncounterBackground &defaultBackground = backgrounds.plain;
+
+  switch (terrain) {
+  case Terrain::bridge:
+    return backgrounds.bridge;
+  case Terrain::mountain:
+    return backgrounds.mountain;
+  case Terrain::plain:
+    return backgrounds.plain;
+  case Terrain::road:
+    return backgrounds.road;
+  case Terrain::sea:
+    return defaultBackground; // Should never happen
+  case Terrain::woods:
+    return backgrounds.woods;
+  case Terrain::invalid:
+    return defaultBackground; // Should never happen
+  }
+
+  // Should never happen
+  return defaultBackground;
+}
 
 static void drawBackgroundSlices(Background &dst,
                                  const EncounterBackground &src, u16 xWords,
@@ -24,11 +51,21 @@ static void drawBackground(Background &dst, const EncounterBackground &src,
 
 void drawMissionEncounter(Background &background, FrameFast &frameFast,
                           const Mission &mission) {
-  // TODO Get these from the terrain types.
-  const EncounterBackground &leftBackground =
-      chip.mission.encounter.backgrounds.plain;
-  const EncounterBackground &rightBackground =
-      chip.mission.encounter.backgrounds.mountain;
+  // TODO Refactor into more helper functions
+  const MapUnit &attackingUnit = mission.map.units[mission.selectedUnitIndex];
+  const MapUnit &defendingUnit = mission.map.units[mission.defendingUnitIndex];
+  const MapUnit &leftUnit =
+      attackingUnit.force == Force::north ? attackingUnit : defendingUnit;
+  const MapUnit &rightUnit =
+      attackingUnit.force == Force::north ? defendingUnit : attackingUnit;
+  const TileIndex leftTileIndex =
+      mission.map.tiles[leftUnit.coords.row][leftUnit.coords.column];
+  const TileIndex rightTileIndex =
+      mission.map.tiles[rightUnit.coords.row][rightUnit.coords.column];
+  const Terrain leftTerrain = tileTerrain(leftTileIndex);
+  const Terrain rightTerrain = tileTerrain(rightTileIndex);
+  const EncounterBackground &leftBackground = terrainBackground(leftTerrain);
+  const EncounterBackground &rightBackground = terrainBackground(rightTerrain);
   constexpr u16 leftXWords = 0;
   constexpr u16 rightXWords = screenWidthInWords / 2;
 
