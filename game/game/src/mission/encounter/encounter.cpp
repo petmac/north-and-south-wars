89c8e259fac93d12b89cc5796b77722cfda2e8ca
Peter Mackay <git@petmac.dev>
2024-10-20 23:46:08 +0100

    Fix incorrect people count for odd HP

--- game/game/src/mission/encounter/encounter.cpp
+++ game/game/src/mission/encounter/encounter.cpp
@@ -21,8 +21,8 @@ void startEncounter(Encounter &encounter, const MapUnit &attackingUnit,
   encounter.frameCounter = 0;
 
   const bool attackerIsOnLeft = attackingUnit.force == Force::north;
-  const u16 attackingPeopleCount = attackingUnit.health / 2;
-  const u16 defendingPeopleCount = defendingUnit.health / 2;
+  const u16 attackingPeopleCount = (attackingUnit.health + 1) / 2;
+  const u16 defendingPeopleCount = (defendingUnit.health + 1) / 2;
   const u16 leftPeopleCount =
       attackerIsOnLeft ? attackingPeopleCount : defendingPeopleCount;
   const u16 rightPeopleCount =
