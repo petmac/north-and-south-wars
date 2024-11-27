32ba1ce6f6f1a8020d0230fa23f38df9db0ba690
Peter Mackay <git@petmac.dev>
2024-11-27 22:58:40 +0000

    Tweak title message

--- game/main/draw_title.cpp
+++ game/main/draw_title.cpp
@@ -9,7 +9,7 @@
 #include "game/title.h"
 
 static constexpr u16 partHeight = 8;
-static constexpr u16 y = screenHeight - partHeight - 8;
+static constexpr u16 y = screenHeight - partHeight - 12;
 
 template <u16 len>
 static void drawCenterText(Background &background, const char (&text)[len]) {
@@ -27,17 +27,43 @@ void drawTitle(Background &background, FrameFast &frameFast,
     frameFast.state = FrameState::drawnTitle;
   }
 
-  switch ((title.frameIndex >> 7) & 3) {
+  switch ((title.frameIndex >> 7) & 15) {
   case 0:
+  case 1:
     drawCenterText(background, "(c) Peter Mackay 2024");
     break;
-  case 1:
-    drawCenterText(background, "A game for AmiGameJam 2024");
-    break;
   case 2:
-    drawCenterText(background, "- Click mouse button to start -");
     break;
   case 3:
+  case 4:
+    drawCenterText(background, "A small game for AmiGameJam 2024");
+    break;
+  case 5:
+    break;
+  case 6:
+  case 7:
+    drawCenterText(background, "- Click mouse button to start -");
+    break;
+  case 8:
+    break;
+  case 9:
+    drawCenterText(background, "Greetings to the AmigaDev,");
+    break;
+  case 10:
+    drawCenterText(background, "AmigaGameDev, and Official");
+    break;
+  case 11:
+    drawCenterText(background, "Commodore Amiga Discords");
+    break;
+  case 12:
+    break;
+  case 13:
+    drawCenterText(background, "Shout out to CharlieFar, NAG_Graham");
+    break;
+  case 14:
+    drawCenterText(background, "& djh0ffman on Twitch");
+    break;
+  case 15:
     break;
   }
 }
